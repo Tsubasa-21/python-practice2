2dcc42c4fe7d736762685d836f8eff3ba6d829ad
Tsubasa-21 <tsubasa.sugizaki1225@gmail.com>
2026-03-31 11:21:41 +0900

    Update day1pra.py

--- day1pra.py
+++ day1pra.py
@@ -2,6 +2,9 @@ def f(x):
     for i in range(0,x):
         a=[]
         a.append(i)
+        print(a)
 
 print(f(3))
+print("hello world")
 # なぜかvscodeだと実行できない？
+# このままだとからのリストにアペンドされたてないのに加えて、返り値がNONEになってしまっている。
