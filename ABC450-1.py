6323a7b6b2e3ba5a021bc52e9feeb92da0abe350
Tsubasa-21 <tsubasa.sugizaki1225@gmail.com>
2026-04-03 12:43:08 +0900

    Update ABC450-1.py

--- ABC450-1.py
+++ ABC450-1.py
@@ -1,2 +1,10 @@
 n=int(input())
-a=[list(map(int,input().split()))for _ in range(n)]
+C=[[None]*(i+1)+list(map(int,input().split()))for i in range(n-1)]
+print(C)
+ans="No"
+for a in range(n):
+    for b in range(a+1,n):#ここでa+1なのはaよりも小さい値をbに代入しない様にするため、cのときも同じ
+        for c in range(b+1,n):
+            if C[a][b]+C[b][c] < C[a][c]:
+                ans="Yes"
+print(ans)
